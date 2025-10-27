6d863c613a10b82beb1a8d4786677b324ec3cf8f
Aníbal Rojas <anibalrojas@gmail.com>
2025-10-26 20:09:59 -0500

    Quartz sync: Oct 26, 2025, 8:09 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -25,7 +25,7 @@ export default (() => {
     const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
-    const iconPath = joinSegments(baseDir, "static/icon.png")
+    const faviconPath = joinSegments(baseDir, "favicon.ico")
 
     // Url of current page
     const socialUrl =
@@ -82,7 +82,7 @@ export default (() => {
           </>
         )}
 
-        <link rel="icon" href={iconPath} />
+        <link rel="icon" href={faviconPath} />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
 
